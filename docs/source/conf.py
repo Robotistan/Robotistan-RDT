4ef5840b1dd1d1ab6102c3cbd5bfd30ae6160e5d
Samet <111511331+amet7@users.noreply.github.com>
2022-11-29 09:52:31 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,23 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information
 
 project = 'PicoBricks'
-copyright = '2022, R&D TEAM'
+copyright = '2022, Robotistan'
 author = 'R&D TEAM'
 
 release = '0.1'
@@ -33,3 +47,17 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'default'
+html_logo = '_static/logo_picobricks.png'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Style
+# pygments_style = "sphinx"
